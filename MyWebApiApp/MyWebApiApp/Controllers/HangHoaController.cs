aa855dc015be3c07f46c47a600e9fed696525b4c
Nguyễn Quốc Huy <95222241+NguyenHuyGit@users.noreply.github.com>
2023-03-03 14:11:40 +0700

    Create entity, update DB

---
+++ MyWebApiApp/MyWebApiApp/Controllers/HangHoaController.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyWebApiApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyWebApiApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HangHoaController : ControllerBase
+    {
+        public static List<HangHoa> hangHoas = new List<HangHoa>();
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(hangHoas);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(string id)
+        {
+            try
+            {
+                //LINQ Queries
+                var hangHoa = hangHoas.SingleOrDefault(hh => hh.MaHangHoa == Guid.Parse(id));
+
+                if (hangHoa == null)
+                {
+                    return NotFound();
+                }
+                return Ok(hangHoa);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+
+        }
+
+        [HttpPost]
+        public IActionResult Create(HangHoaVM hangHoaVM)
+        {
+            var hangHoa = new HangHoa
+            {
+                MaHangHoa = Guid.NewGuid(),
+                TenHangHoa = hangHoaVM.TenHangHoa,
+                DonGia = hangHoaVM.DonGia
+            };
+
+            hangHoas.Add(hangHoa);
+
+            return Ok(new
+            {
+                Success = true, Data = hangHoa
+            });
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Edit(string id, HangHoa hangHoaEdit)
+        {
+            try
+            {
+                //LINQ Queries
+                var hangHoa = hangHoas.SingleOrDefault(hh => hh.MaHangHoa == Guid.Parse(id));
+
+                if (hangHoa == null)
+                {
+                    return NotFound();
+                }
+
+                if (id != hangHoa.MaHangHoa.ToString())
+                {
+                    return BadRequest();
+                }
+
+                // Update
+                hangHoa.TenHangHoa = hangHoaEdit.TenHangHoa;
+                hangHoa.DonGia = hangHoaEdit.DonGia;
+
+                return Ok();
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Remove(string id)
+        {
+            try
+            {
+                //LINQ Queries
+                var hangHoa = hangHoas.SingleOrDefault(hh => hh.MaHangHoa == Guid.Parse(id));
+
+                if (hangHoa == null)
+                {
+                    return NotFound();
+                }
+                  
+
+                // Update
+                hangHoas.Remove(hangHoa); 
+                return Ok();
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ MyWebApiApp/MyWebApiApp/Controllers/LoaisController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyWebApiApp.Data;
+using MyWebApiApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyWebApiApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoaisController : ControllerBase
+    {
+        private readonly MyDbContext _context;
+        public LoaisController(MyDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var DsLoai = _context.Loais.ToList();
+            return Ok(DsLoai);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetByID(int id)
+        {
+            var loai = _context.Loais.SingleOrDefault(lo => lo.MaLoai == id);
+
+            if (loai != null)
+            {
+                return Ok(loai);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPost]
+        public IActionResult CreateNew(LoaiModel model)
+        {
+            try
+            {
+                var loai = new Loai
+                {
+                    TenLoai = model.TenLoai
+                };
+
+                _context.Add(loai);
+                _context.SaveChanges();
+                return Ok(loai);
+            }
+            catch
+            {
+                return BadRequest();
+            }                       
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, LoaiModel model)
+        {
+            var loai = _context.Loais.SingleOrDefault(lo => lo.MaLoai == id);
+
+            if (loai!=null)
+            {
+                loai.TenLoai = model.TenLoai;
+                _context.SaveChanges();
+                return NoContent();
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
